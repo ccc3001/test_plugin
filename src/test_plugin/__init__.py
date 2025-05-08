3778b205dbfd35f6e817949a548ea03062ab2011
ccc3001 <69085353+ccc3001@users.noreply.github.com>
2025-05-08 15:20:14 +0200

    Update __init__.py

--- src/test_plugin/__init__.py
+++ src/test_plugin/__init__.py
@@ -1,18 +1,1 @@
-from nomad.config.models.plugins import ParserEntryPoint
-from pydantic import Field
 
-
-class MyParserOneEntryPoint(ParserEntryPoint):
-    parameter: int = Field(0, description="Custom configuration parameter")
-
-    def load(self):
-        from nomad_aa_plugin.parsers.parser import MyParserOne
-
-        return MyParserOne(**self.dict())
-
-
-parser_one_entry_point = MyParserOneEntryPoint(
-    name="MyParserOne",
-    description="My parser entry point configuration.",
-    mainfile_name_re=r".+\.nxs",
-)
