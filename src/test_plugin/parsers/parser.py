09d68148491a6a8067faa4ae967a57c46dcc6080
ccc3001 <johann.strunck@gmx.de>
2025-06-12 09:52:04 +0200

    added a new schema, now it should be possible that html files are added automaitcaly which can be used for displaying reports

--- src/test_plugin/parsers/parser.py
+++ src/test_plugin/parsers/parser.py
@@ -3,11 +3,9 @@ from typing import (
     TYPE_CHECKING,
 )
 
-if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
+
+from nomad.datamodel.datamodel import EntryArchive
+from structlog.stdlib import (
         BoundLogger,
     )
 
@@ -19,7 +17,16 @@ from test_plugin.schema_packages.schema_package import NewSchemaPackage
 configuration = config.get_plugin_entry_point(
     'test_plugin.parsers:parser_entry_point'
 )
-archive.data = NewSchemaPackage()
+
+#from nomad.datamodel.context import ServerContext
+
+#from reportlab.pdfgen import canvas
+#from io import BytesIO
+#pdf_buffer = BytesIO()
+#pdf = canvas.Canvas(pdf_buffer)
+#pdf.save()
+#pdf_buffer.seek(0) 
+
 
 class NewParser(MatchingParser):
     def parse(
@@ -29,6 +36,7 @@ class NewParser(MatchingParser):
         logger: 'BoundLogger',
         child_archives: dict[str, 'EntryArchive'] = None,
     ) -> None:
+        archive.data= NewSchemaPackage()
         logger.info('NewParser.parse', parameter=configuration.parameter)
         with h5py.File(mainfile, "r") as f:
             if "demo_instrument_detectorZ" in list(f["CAMELS_entry"]["data"].keys()):
@@ -36,3 +44,9 @@ class NewParser(MatchingParser):
                 archive.data.detectorZ =f["CAMELS_entry"]["data"]["demo_instrument_detectorZ"][()]
             archive.data.certain_value= "test"
         logger.info("h5 was read propperly")
+
+        #creating the pdf
+        #output = f'test.html'
+        #with archive.m_context.raw_file(output,'w') as outfile:
+        #    outfile.write("test")#pdf_buffer.read())
+        #archive.data.results_pdf = output
--- src/test_plugin/schema_packages/schema_package.py
+++ src/test_plugin/schema_packages/schema_package.py
@@ -1,4 +1,3 @@
-
 from nomad.config import config
 from nomad.datamodel.data import EntryData,ArchiveSection
 from nomad.metainfo import Quantity, SchemaPackage
@@ -6,11 +5,33 @@ from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
 import numpy as np
 
 configuration = config.get_plugin_entry_point(
-    'nomad_plugin_test.schema_packages:schema_package_entry_point'
+    'test_plugin.schema_packages:schema_package_entry_point'
 )
 
 m_package = SchemaPackage()
 
+
+
+
+
+import base64
+import io 
+def fig_to_base64(fig):
+    img =io.BytesIO()
+    fig.savefig(img,format='png',bbox_inches='tight')
+    img.seek(0)
+
+
+html_begining= """<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Test report</h1>
+<p>This is the description of the report<p>"""
+
+html_end="""</body>
+</html>"""
 
 # Define your schema class
 class NewSchemaPackage(ArchiveSection):
@@ -20,3 +41,24 @@ class NewSchemaPackage(ArchiveSection):
     certain_value = Quantity(
         type=str
     )
+    results_pdf = Quantity(
+        type=str,
+         a_browser=dict(adaptor='RawFileAdaptor')
+    )
+    import matplotlib.pyplot as plt
+    from reportlab.pdfgen import canvas
+    from io import BytesIO
+    pdf_buffer = BytesIO()
+    pdf = canvas.Canvas(pdf_buffer)
+    pdf.save()
+    pdf_buffer.seek(0) 
+
+    def normalize(self, archive:'EntryArchive',logger:'BoundLogger')-> None:
+        super(NewSchemaPackage,self).normalize(archive,logger)
+        output=f"test.html"
+        fig =fig = plt.figure()
+        encoded = fig_to_base64(fig)
+        my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+        with archive.m_context.raw_file(output,'w') as file:
+            file.write(html_begining+my_html+html_end)#self.pdf_buffer.read())
+        results_pdf = output
