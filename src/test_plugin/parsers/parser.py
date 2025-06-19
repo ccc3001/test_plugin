397de167814d1cc15867d92d126b180b94cb4866
ccc3001 <johann.strunck@gmx.de>
2025-06-19 16:13:53 +0200

    added a few new variables to the schema file, updated the normalizer to display a more sofisticated html and also wrote a function for loading all the variables from hdf5 and nxs files

--- src/test_plugin/parsers/parser.py
+++ src/test_plugin/parsers/parser.py
@@ -18,17 +18,20 @@ configuration = config.get_plugin_entry_point(
     'test_plugin.parsers:parser_entry_point'
 )
 
-#from nomad.datamodel.context import ServerContext
-
-#from reportlab.pdfgen import canvas
-#from io import BytesIO
-#pdf_buffer = BytesIO()
-#pdf = canvas.Canvas(pdf_buffer)
-#pdf.save()
-#pdf_buffer.seek(0) 
-
 
 class NewParser(MatchingParser):
+    def set_atribute(self,value,_object,_atribute,keys):
+        for key in keys[:-1]:
+            value=value[key]
+            
+        if keys[-1] in list(value.keys()):
+            value=value[keys[-1]][()]
+            if isinstance(value, bytes):
+                setattr(_object,_atribute,value.decode('utf-8'))
+            else:
+                setattr(_object,_atribute,value)    
+        else:
+            setattr(_object,_atribute,None)
     def parse(
         self,
         mainfile: str,
@@ -38,15 +41,21 @@ class NewParser(MatchingParser):
     ) -> None:
         archive.data= NewSchemaPackage()
         logger.info('NewParser.parse', parameter=configuration.parameter)
+        
+        
+        #for instrument in f["CAMELS_entry"]["user"]:
+        #    instrument_names= instrument_names
         with h5py.File(mainfile, "r") as f:
-            if "demo_instrument_detectorZ" in list(f["CAMELS_entry"]["data"].keys()):
-                logger.info("finding detector")
-                archive.data.detectorZ =f["CAMELS_entry"]["data"]["demo_instrument_detectorZ"][()]
+            self.set_atribute(f ,archive.data, "measurement_comments" , ["CAMELS_entry" "measurement_details" "measurement_comments"])
+            self.set_atribute(f,archive.data,"measurement_description",["CAMELS_entry","measurement_details","measurement_description"])
+            self.set_atribute(f,archive.data,"protocol_description",["CAMELS_entry","measurement_details","protocol_description"])
+            self.set_atribute(f,archive.data,"time",["CAMELS_entry","data","time"])
+            self.set_atribute(f,archive.data,"ElapsedTime",["CAMELS_entry","data","ElapsedTime"])
+            self.set_atribute(f,archive.data,"first_name",["CAMELS_entry","user","first_name"])
+            self.set_atribute(f,archive.data,"last_name",["CAMELS_entry","user","last_name"])
+            self.set_atribute(f,archive.data,"email",["CAMELS_entry","user","email"])
+            self.set_atribute(f,archive.data,"affiliation",["CAMELS_entry","user","affiliation"])
+            self.set_atribute(f,archive.data,"detectorZ",["CAMELS_entry","data","demo_instrument_detectorZ"])
             archive.data.certain_value= "test"
         logger.info("h5 was read propperly")
         logger.info(str(os.getcwd()))
-        #creating the pdf
-        #output = f'test.html'
-        #with archive.m_context.raw_file(output,'w') as outfile:
-        #    outfile.write("test")#pdf_buffer.read())
-        #archive.data.results_pdf = output
--- src/test_plugin/schema_packages/schema_package.py
+++ src/test_plugin/schema_packages/schema_package.py
@@ -23,16 +23,11 @@ def fig_to_base64(fig):
     img.seek(0)
     return base64.b64encode(img.getvalue())
 
-html_begining= """<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<h1>Test report</h1>
-<p>This is the description of the report<p>"""
 
-html_end="""</body>
-</html>"""
+
+
+
+
 
 
 import matplotlib.pyplot as plt
@@ -40,12 +35,37 @@ import matplotlib.pyplot as plt
 
 # Define your schema class
 class NewSchemaPackage(ArchiveSection):
-    detectorZ = Quantity(
-        type=np.float64,
-        shape=['*'])
-    certain_value = Quantity(
-        type=str
+    measurement_comments = Quantity(
+        type= str 
     )
+    measurement_description = Quantity(
+        type = str
+    )
+    protocol_description = Quantity(
+        type = str
+    )
+    time = Quantity(
+        type = np.float64,
+        shape = ['*']
+    )
+    elapsed_time = Quantity(
+        type=np.float64,
+        shape = ['*']
+    )
+    first_name = Quantity(
+        type=str
+    )    
+    last_name = Quantity(
+        type= str 
+    )
+    email =  Quantity(
+        type = str 
+    )
+    affiliation = Quantity(
+        type = str 
+    )
+
+
     results_pdf = Quantity(
         type=str,
         a_browser=dict(adaptor='RawFileAdaptor',render_value ='HtmlValue')
@@ -54,31 +74,68 @@ class NewSchemaPackage(ArchiveSection):
         type= str,
         a_browser=dict(render_value ='HtmlValue')
     )
-    #from reportlab.pdfgen import canvas
-    #from io import BytesIO
-    #pdf_buffer = BytesIO()
-    #pdf = canvas.Canvas(pdf_buffer)
-    #pdf.save()
-    #pdf_buffer.seek(0) 
+    
+    
+    certain_value = Quantity(
+        type=str
+    )
+    detectorZ = Quantity(
+        type=np.float64,
+        shape=['*'])
+
+
+    def create_pdf(self,image):
+        minimum = np.min(self.detectorZ)
+        maximum = np.max(self.detectorZ)
+        average =np.average(self.detectorZ)
+        mean = np.mean(self)
+        return f"""
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#outline {{
+    outline: 2px solid black;
+}}
+
+</style>
+</head>
+<body>
+<h1>Test report</h1>
+
+<p id="outline"> Researcher: {self.first_name} {self.last_name}
+<br>Email: {self.email}
+<br>Institute: {self.affiliation}</p>
+
+
+<h2>Protocol description </h2>
+<p id="outline"><br>{self.protocol_description}<br><br><p><br>
+<h2>Measurement description </h2>
+<p id="outline"><br>{self.measurement_description}<br><br><p><br>
+{image}
+<p id="outline"><br>min: {minimum} max: {maximum} <br>mean: {mean} average: {average} <br><br></p>
+<h2>Measurement Comments</h2>
+
+<p id="outline"><br>{self.measurement_comments}<br><br><p>
+</body>
+</html>
+"""
 
     def normalize(self, archive:'EntryArchive',logger:'BoundLogger')-> None:
         super(NewSchemaPackage,self).normalize(archive,logger)
         output=f"test.html"
         if self.detectorZ is not None:
-            minimum = np.min(self.detectorZ)
-            maximum = np.max(self.detectorZ)
-            average =np.average(self.detectorZ)
             html_description=f"""
             <p>MaxTemp:{maximum}<br> MinTemp:{minimum}<br> AvgTemp:{average}</p>
              """
             fig,ax = plt.subplots()
-            ax.plot(self.detectorZ)
+            ax.plot(self.ElapsedTime , self.detectorZ)
             ax.set_title("Simple Line Plot")
             ax.set_xlabel("X-axis")
             ax.set_ylabel("Y-axis")
             encoded = fig_to_base64(fig)
-            my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+            image_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+            final_html = create_pdf(self,image_html)
             with archive.m_context.raw_file(output,'w') as file:
-                file.write(html_begining+"\n"+my_html+"\n"+html_description+"\n"+html_end)#self.pdf_buffer.read())
-            results_pdf = output
-            self.results_html=str(html_begining+"\n"+my_html+"\n"+html_description+"\n"+html_end)
+                file.write(final_html)
+            self.results_html=str(final_html)
